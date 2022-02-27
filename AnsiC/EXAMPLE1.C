dd867b7542557badd7b7bcf91d70878ca92808df
Bharani Amarnath <bharaniamarnath@gmail.com>
2022-02-27 17:20:51 +0530

    Ansi C Commit #27022022

---
+++ AnsiC/EXAMPLE1.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int a,b,c;
+clrscr();
+c=a+b;
+printf("Enter A: ");
+scanf("%d",&a);
+printf("Enter B: ");
+scanf("%d",&b);
+printf("The result is %d",&c);
+getch();
+}
