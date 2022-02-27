52d636cdefb8e3b1f01735ae717dd9934d882985
Bharani Amarnath <bharaniamarnath@gmail.com>
2022-02-27 17:30:13 +0530

    Visual Basic Commit #27022022

---
+++ VisualBasic/example1/example1/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Calculate = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'Calculate
+        '
+        Me.Calculate.Location = New System.Drawing.Point(208, 233)
+        Me.Calculate.Name = "Calculate"
+        Me.Calculate.Size = New System.Drawing.Size(75, 23)
+        Me.Calculate.TabIndex = 0
+        Me.Calculate.Text = "Calculate"
+        Me.Calculate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(295, 268)
+        Me.Controls.Add(Me.Calculate)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Calculate As System.Windows.Forms.Button
+
+End Class
---
+++ VisualBasic/example1/example1/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Calculate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Calculate.Click
+        Dim num1, num2, sum As Single
+        num1 = 100
+        num2 = 200
+        sum = num1 + num2
+        MsgBox("The sum of " & num1 & " and" & num2 & "is " & sum)
+
+    End Sub
+End Class
